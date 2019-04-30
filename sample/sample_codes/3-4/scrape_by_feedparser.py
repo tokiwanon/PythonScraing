ac783f15ce15430c410f4cb52b9d5a387b3ab41f
Murayama Sashiro <m.sasirou@gmail.com>
2019-04-30 09:40:51 +0900

    fifth commit

--- sample/sample_codes/3-4/scrape_by_feedparser.py
+++
@@ -1,8 +0,0 @@
-import feedparser
-
-# はてなブックマークの人気エントリー（「テクノロジー」カテゴリ）のRSSを読み込む。
-d = feedparser.parse('http://b.hatena.ne.jp/hotentry/it.rss')
-
-# すべての要素について処理を繰り返す。
-for entry in d.entries:
-    print(entry.link, entry.title)
--- sample/sample_codes/3-7/python_crawler_1.py
+++
@@ -1,8 +0,0 @@
-import requests
-import lxml.html
-
-response = requests.get('https://gihyo.jp/dp')
-root = lxml.html.fromstring(response.content)
-for a in root.cssselect('a[itemprop="url"]'):
-    url = a.get('href')
-    print(url)
